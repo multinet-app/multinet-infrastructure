3d12a9835f8767cc7c01f97984a9d7a38cfa1425
Roni Choudhury <roni.choudhury@kitware.com>
2021-04-22 11:25:07 -0400

    Initial attempt at adding terraform infrastructure
    This was copied and adapted from the dandi/dandi-infrastructure
    repository.

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "remote" {
+    organization = "multinet"
+
+    workspaces {
+      name = "multinet-prod"
+    }
+  }
+}
+
+// This is the "project" account, the primary account with most resources
+provider "aws" {
+  region              = "us-east-2"
+  allowed_account_ids = ["201301540100"]
+  # Must set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY envvars
+}
+
+provider "heroku" {
+  # Must set HEROKU_EMAIL, HEROKU_API_KEY envvars
+}
+
+data "aws_caller_identity" "project_account" {}
