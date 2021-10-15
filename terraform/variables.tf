d5e31235bdcee2e902ad32fcfb8830e98ad7a830
JackWilb <jackwilburn@tutanota.com>
2021-10-15 13:25:58 -0600

    Add missing required variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,23 @@ variable "MULTINET_ARANGO_READONLY_PASSWORD" {
     type = string
     sensitive = true
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+    sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+    sensitive = true
+}
+
+variable "HEROKU_API_KEY" {
+    type = string
+    sensitive = true
+}
+
+variable "HEROKU_EMAIL" {
+    type = string
+    sensitive = true
+}
