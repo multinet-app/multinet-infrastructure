90bcd0bcb05e93e080e7ff8f3a23c6d4220bb779
JackWilb <jackwilburn@tutanota.com>
2022-01-19 14:57:08 -0700

    Fix deploy preview regex, add multidynamic

--- terraform/api.tf
+++ terraform/api.tf
@@ -15,7 +15,7 @@ module "api" {
   subdomain_name   = "api"
 
   django_cors_origin_whitelist = ["https://multinet.app", "https://multimatrix.multinet.app", "https://multilink.multinet.app", "https://multidynamic.multinet.app", "https://vdl.sci.utah.edu"]
-  django_cors_origin_regex_whitelist = ["^https:\\/\\/deploy-preview-[0-9a-z\\-]+--next-multinet-client\\.netlify\\.app$", "^https:\\/\\/deploy-preview-[0-9a-z\\-]+--next-multimatrix\\.netlify\\.app$", "^https:\\/\\/deploy-preview-[0-9a-z\\-]+--next-multilink\\.netlify\\.app$"]
+  django_cors_origin_regex_whitelist = ["^https:\\/\\/deploy-preview-[0-9a-z\\-]+--multinet-client\\.netlify\\.app$", "^https:\\/\\/deploy-preview-[0-9a-z\\-]+--multimatrix\\.netlify\\.app$", "^https:\\/\\/deploy-preview-[0-9a-z\\-]+--next-multilink\\.netlify\\.app$", "^https:\\/\\/deploy-preview-[0-9a-z\\-]+--multidynamic\\.netlify\\.app$"]
   
 
   additional_sensitive_django_vars = {
